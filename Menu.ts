38b7f7a36f07bcea390db9cd1dabfec9d7684bec
Samuel F <samuka2568@gmail.com>
2024-12-12 21:07:58 -0300

    :sparkles: Classe Model Conta Corrente e Conta Poupança - Com Herança, Polimorfismo e Métodos

--- Menu.ts
+++ Menu.ts
@@ -1,21 +1,59 @@
 import readlinesync = require("readline-sync");
 import { colors } from "./src/util/Colors";
 import { Conta } from "./src/model/Conta";
+import { ContaCorrente } from "./src/model/ContaCorrente";
+import { ContaPoupanca } from "./src/model/ContaPoupanca";
 
 export function main() {
   let opcao: number;
 
-  // Cria novas instâncias (Objetos) da Classe Conta
-  const c1 = new Conta(1, 1, 1, "Paulo", 1000);
-  c1.visualizar();
-  c1.trocaConta(2);
-  c1.visualizar();
+  // //  Cria novas instâncias (Objetos) da Classe Conta
+  // const c1 = new Conta(1, 1, 1, "Paulo", 1000);
+  // c1.visualizar();
+  // c1.trocaConta(2);
+  // c1.visualizar();
 
-  const c2 = new Conta(1, 0o1, 2, "Aline", 200000);
-  c2.visualizar();
-  c2.depositar(2000);
-  c2.sacar(1000);
-  c2.visualizar();
+  // const c2 = new Conta(2, 1, 2, "Aline", 200000);
+  // c2.visualizar();
+  // c2.depositar(2000);
+  // c2.sacar(1000);
+  // c2.visualizar();
+  // c2.trocaConta(1);
+  // c2.visualizar();
+
+  // // Contas Correntes
+
+  const contac = new ContaCorrente(3, 789, 1, "Andressa", 100000, 1000);
+  contac.visualizar();
+  contac.sacar(100500);
+  contac.visualizar();
+  contac.depositar(2000);
+  contac.visualizar;
+  console.log("");
+
+  const contaC1 = new ContaCorrente(4, 352, 1, "Joana", 5000, 1000);
+  contaC1.visualizar();
+  contaC1.sacar(220);
+  contaC1.visualizar();
+  contaC1.depositar(1000);
+  contaC1.visualizar;
+  console.log("");
+
+  const contap = new ContaPoupanca(5, 300, 2, "Thiago", 1412, 28);
+  contap.visualizar();
+  contap.sacar(2500);
+  contap.visualizar();
+  contap.depositar(1000);
+  contap.visualizar();
+  console.log("");
+
+  const contap1 = new ContaPoupanca(6, 300, 2, "William", 5000, 28);
+  contap1.visualizar();
+  contap1.depositar(1000);
+  contap1.visualizar();
+  contap1.sacar(5500);
+  contap1.visualizar();
+  console.log("");
 
   while (true) {
     console.log(
